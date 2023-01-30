8bae6625517a10b740bc50f88b7471dca0192683
jihee-yun <dandelion1141@gmail.com>
2023-01-30 21:36:15 +0900

    230130

--- src/해시맵커피/CoffeeMenuEx.java
+++ src/해시맵커피/CoffeeMenuEx.java
@@ -1,8 +1,13 @@
 package 해시맵커피;
 
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 import java.util.Scanner;
+// HashMap? 키와 값 pair 로 구성된 형태
+// 동일 여부 판단을 HashCode 를 통해서 진행
+// 키는 중복 허용하지 않지만 값은 중복 허용
+// 순서롤 보장하지 않는다
 
 public class CoffeeMenuEx {
     Map<String, MenuInfo> map = new HashMap<>();
@@ -25,8 +30,8 @@ public class CoffeeMenuEx {
             switch (selMenu) {
                 case 1:
                     System.out.println("======== 메뉴 보기 ========");
-                    for(String e : map.keySet()) {
-                        System.out.println("메뉴 : " + map.get(e).name);
+                    for(String e : map.keySet()) { // 키값 기준으로 향상된 for문 반복 수행
+                        System.out.println("메뉴 : " + map.get(e).name); // 키로 해당하는 값을 얻어 냄
                         System.out.println("가격 : " + map.get(e).price);
                         System.out.println("분류 : " + map.get(e).group);
                         System.out.println("설명 : " + map.get(e).desc);
@@ -36,7 +41,7 @@ public class CoffeeMenuEx {
                 case 2:
                     System.out.print("조회할 메뉴를 입력하세요 : ");
                     key = sc.next();
-                    if(map.containsKey(key)) {
+                    if(map.containsKey(key)) { // 매개변수로 전달된 키가 Map 내에 존재하는지 확인 (true/false)
                         System.out.println("메뉴 : " + map.get(key).name);
                         System.out.println("가격 : " + map.get(key).price);
                         System.out.println("분류 : " + map.get(key).group);
@@ -54,13 +59,38 @@ public class CoffeeMenuEx {
                         int price = sc.nextInt();
                         System.out.print("분류 : ");
                         String group = sc.next();
+                        sc.nextLine();
                         System.out.print("설명 : ");
-                        String desc = sc.next();
-                        map.put(key, new MenuInfo(key, price, group, desc));
+                        String desc = sc.nextLine();
+                        map.put(key, new MenuInfo(key, price, group, desc)); // 키와 값(객체)을 추가
                     }
                     break;
                 case 4:
+                    System.out.print("삭제할 메뉴를 입력하세요 : ");
+                    key = sc.next();
+                    if(map.containsKey(key)) { // 삭제할 메뉴가 있는지 확인
+                        map.remove(key); // 키로 해당하는 맵의 entry 제거
+                        System.out.println(key + " 메뉴를 삭제하였습니다.");
+                    } else {
+                        System.out.println("삭제할 메뉴가 없습니다.");
+                    }
+                    break;
                 case 5:
+                    System.out.print("수정할 메뉴를 입력하세요 : ");
+                    key = sc.next();
+                    if(map.containsKey(key)) {
+                        System.out.print("가격 : ");
+                        int price = sc.nextInt();
+                        System.out.print("분류 : ");
+                        String group = sc.next();
+                        sc.nextLine(); // 버퍼 비우기
+                        System.out.print("설명 : ");
+                        String desc = sc.nextLine(); // 공백을 입력 받기 위해서
+                        map.replace(key, new MenuInfo(key, price, group,desc));
+                    } else {
+                        System.out.println("수정할 메뉴가 없습니다.");
+                    }
+                    break;
                 case 6:
                     System.out.println("메뉴를 종료합니다.");
                     return;
